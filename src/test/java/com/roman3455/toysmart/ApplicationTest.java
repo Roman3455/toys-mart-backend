32b988082585d8ac7b88eee0786e2db7f94858c6
Roman Smirnov <s.roman3455@gmail.com>
2026-01-25 23:46:34 +0300

    init: project initialization

---
+++ src/test/java/com/roman3455/toysmart/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.roman3455.toysmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
