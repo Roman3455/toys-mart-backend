bad6e5991def7d388bdae671f310588be510ea84
Roman Smirnov <s.roman3455@gmail.com>
2026-01-26 21:54:00 +0300

    feat: add dummy Product controller
    - stab getProduct method

---
+++ src/main/java/com/roman3455/toysmart/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.roman3455.toysmart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "List of products";
+    }
+
+}
