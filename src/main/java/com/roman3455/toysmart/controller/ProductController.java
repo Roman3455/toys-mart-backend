07d820fc26aae8505cf599f1f926ea29d51b9827
Roman Smirnov <s.roman3455@gmail.com>
2026-01-28 21:57:27 +0300

    feat: add getProducts method

--- src/main/java/com/roman3455/toysmart/controller/ProductController.java
+++ src/main/java/com/roman3455/toysmart/controller/ProductController.java
@@ -1,16 +1,26 @@
 package com.roman3455.toysmart.controller;
 
+import com.roman3455.toysmart.entity.Product;
+import com.roman3455.toysmart.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
-    public String getProducts() {
-        return "List of products";
+    public List<Product> getProducts() {
+        return productService.getAllProducts();
     }
 
 }
