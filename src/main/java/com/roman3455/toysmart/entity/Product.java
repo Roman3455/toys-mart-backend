9fac7789203676c60a566f59b1942560b48c34ef
Roman Smirnov <s.roman3455@gmail.com>
2026-01-28 23:33:49 +0300

    refactor: update Product entity

--- src/main/java/com/roman3455/toysmart/entity/Product.java
+++ src/main/java/com/roman3455/toysmart/entity/Product.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -16,34 +17,37 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product_id", nullable = false)
+    @Column(name = "PRODUCT_ID", nullable = false)
     private Long productId;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "NAME", nullable = false, length = 250)
     private String name;
 
-    @Column(name = "description", nullable = false)
+    @Column(name = "DESCRIPTION", nullable = false, length = 500)
     private String description;
 
-    @Column(name = "price", nullable = false)
+    @Column(name = "PRICE", nullable = false, precision = 10, scale = 2)
     private BigDecimal price;
 
-    @Column(name = "popularity", nullable = false)
+    @Column(name = "POPULARITY", nullable = false)
     private Integer popularity;
 
-    @Column(name = "image_url")
+    @Column(name = "IMAGE_URL", length = 500)
     private String imageUrl;
 
-    @Column(name = "created_at", nullable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "CREATED_AT", nullable = false)
     private Instant createdAt;
 
-    @Column(name = "created_by", nullable = false)
+    @Column(name = "CREATED_BY", nullable = false, length = 20)
     private String createdBy;
 
-    @Column(name = "updated_at")
+    @ColumnDefault("NULL")
+    @Column(name = "UPDATED_AT")
     private Instant updatedAt;
 
-    @Column(name = "updated_by")
+    @ColumnDefault("NULL")
+    @Column(name = "UPDATED_BY", length = 20)
     private String updatedBy;
 
     public Long getProductId() {
@@ -125,4 +129,5 @@ public class Product {
     public void setUpdatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
     }
+
 }
