bc03413f6d6fa4856a7f191d2e8a6dbaa725b00f
Roman Smirnov <s.roman3455@gmail.com>
2026-02-02 21:06:48 +0300

    feat: add ContactDTO

---
+++ src/main/java/com/roman3455/toysmart/dto/ContactRequestDto.java
@@ -0,0 +1,42 @@
+package com.roman3455.toysmart.dto;
+
+public class ContactRequestDto {
+
+    private String name;
+    private String email;
+    private String mobileNumber;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
