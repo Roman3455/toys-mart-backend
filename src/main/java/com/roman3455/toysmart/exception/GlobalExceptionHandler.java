1810c63e6b1791fc18b95027d39ec1a0ec472e66
Roman Smirnov <s.roman3455@gmail.com>
2026-02-06 19:58:58 +0300

    feat: add Validation exception handler

--- src/main/java/com/roman3455/toysmart/exception/GlobalExceptionHandler.java
+++ src/main/java/com/roman3455/toysmart/exception/GlobalExceptionHandler.java
@@ -1,16 +1,25 @@
 package com.roman3455.toysmart.exception;
 
 import com.roman3455.toysmart.dto.ErrorResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleException(RuntimeException ex, WebRequest req) {
@@ -21,6 +30,16 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(dto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException exception) {
+        logger.error("An exception occurred due to : {}", exception.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
+        fieldErrorList.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 
 }
